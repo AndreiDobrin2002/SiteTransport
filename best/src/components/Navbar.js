842a74240c60cadd26fb2fa700919bc361891316
Andrei Dobrin <andreidobrin12@gmail.com>
2025-08-08 19:59:28 +0300

    Modificare si imbunatatire navbar

--- best/src/components/Navbar.js
+++ best/src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./Navbar.css";
 import logo from "../assets/logo2.png";
 import logoScrolled from "../assets/logo.png";
@@ -27,13 +27,13 @@ const Navbar = () => {
 
     return (
         <nav className={`navbar ${scrolled ? "scrolled" : "transparent"}`}>
-            <Link to="/" onClick={closeMenu}>
+            <NavLink to="/" onClick={closeMenu}>
                 <img
                     src={scrolled ? logoScrolled : logo}
                     alt="Logo Firma"
                     className="logo"
                 />
-            </Link>
+            </NavLink>
 
             <button className="burger" onClick={toggleMenu}>
                 <div className={`line ${menuOpen ? "open" : ""}`} />
@@ -42,12 +42,20 @@ const Navbar = () => {
             </button>
 
             <ul className={`nav-links ${menuOpen ? "active" : ""}`}>
-                <li><Link to="/" onClick={closeMenu}>Acasă</Link></li>
-                <li><Link to="/despre" onClick={closeMenu}>Despre noi</Link></li>
-                <li><Link to="/servicii" onClick={closeMenu}>Servicii</Link></li>
-                <li><Link to="/galerie" onClick={closeMenu}>Galerie</Link></li>
-                <li><Link to="/comanda" onClick={closeMenu}>Comandă</Link></li>
-                <li><Link to="/contact" onClick={closeMenu}>Contact</Link></li>
+                <li><NavLink to="/" onClick={closeMenu}>Acasă</NavLink></li>
+                <li><NavLink to="/despre" onClick={closeMenu}>Despre noi</NavLink></li>
+                <li className="dropdown">
+                    <NavLink to="/servicii" onClick={closeMenu}>Servicii</NavLink>
+                    <ul className="dropdown-menu">
+                        <li><NavLink to="/transport-auto" onClick={closeMenu}>Transport auto</NavLink></li>
+                        <li><NavLink to="/logistica-depozitare" onClick={closeMenu}>Logistică și Depozitare</NavLink></li>
+                        <li><NavLink to="/intermedieri-transport" onClick={closeMenu}>Intermediere Transport</NavLink></li>
+                    </ul>
+                </li>
+
+                <li><NavLink to="/galerie" onClick={closeMenu}>Galerie</NavLink></li>
+                <li><NavLink to="/comanda" onClick={closeMenu}>Comandă</NavLink></li>
+                <li><NavLink to="/contact" onClick={closeMenu}>Contact</NavLink></li>
             </ul>
         </nav>
     );
