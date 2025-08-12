76f1cff9faab751a2d5a9184acc02d731bd7526b
Andrei Dobrin <andreidobrin12@gmail.com>
2025-08-12 14:28:10 +0300

    Imbunatatire componenta statistici

--- best/src/components/Statistici.js
+++ best/src/components/Statistici.js
@@ -1,6 +1,7 @@
 import CountUp from "react-countup";
 import "./Statistici.css";
 import { FaTruck, FaCalendarAlt, FaUsers, FaWarehouse } from "react-icons/fa";
+import { useInView } from "react-intersection-observer";
 
 const statistici = [
     { icon: <FaTruck />, number: 6453, suffix: "+", text: "Vehicule transportate" },
@@ -10,13 +11,23 @@ const statistici = [
 ];
 
 export default function Statistici() {
+    const { ref, inView } = useInView({
+        triggerOnce: false, // Setează la true dacă vrei să declanșezi animația doar o singură dată
+        threshold: 0.3,
+    });
+
     return (
-        <div className="statistici-container">
+        <div className="statistici-container" ref={ref}>
             {statistici.map((stat, index) => (
                 <div key={index} className="stat-box">
                     <div className="stat-icon">{stat.icon}</div>
                     <div className="stat-number">
-                        <CountUp end={stat.number} duration={3.5} suffix={stat.suffix || ""} />
+                        {/* Pornește animația doar când inView este true */}
+                        {inView ? (
+                            <CountUp end={stat.number} duration={3.5} suffix={stat.suffix || ""} />
+                        ) : (
+                            0
+                        )}
                     </div>
                     <div className="stat-text">{stat.text}</div>
                 </div>
